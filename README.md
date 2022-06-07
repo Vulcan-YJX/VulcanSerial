c80baa810e630ee041fbdd98e5a5b3099f54a3a4
vulcanai <vulcanai@163.com>
2022-06-07 14:03:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VulcanSerial
+linux Serial library
---
+++ build/CMakeFiles/vulcanserial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulcanserial.dir/main.cpp.o"
+  "vulcanserial"
+  "vulcanserial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulcanserial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
