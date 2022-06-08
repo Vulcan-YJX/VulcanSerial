c7d5f152739271ed4499f74c42fec56354272145
Vulcan-YJX <vulcanyjx@163.com>
2022-06-08 16:23:32 +0800

    change

--- src/SerialPort.cpp
+++ src/SerialPort.cpp
@@ -1,3 +1,4 @@
+
 //!
 //! @file 			SerialPort.cpp
 //! @author 		Vulcan YJX <vulcanai@163.com> 
@@ -5,38 +6,62 @@
 //! @last-modified 	2022-06-07
 //! @brief			The main serial port class.
 
-#include <termios.h> 
+
+// System includes
+#include <iostream>
+#include <sstream>
+#include <stdio.h>   	// Standard input/output definitions
+#include <string.h>  	// String function definitions
 #include <unistd.h>  	// UNIX standard function definitions
+#include <fcntl.h>   	// File control definitions
+#include <errno.h>   	// Error number definitions
+// #include <termios.h> 	// POSIX terminal control definitions (struct termios)
 #include <system_error>	// For throwing std::system_error
 #include <sys/ioctl.h> // Used for TCGETS2, which is required for custom baud rates
+#include <cassert>
+// #include <asm/termios.h> // Terminal control definitions (struct termios)
+#include <asm/ioctls.h>
+#include <asm/termbits.h>
+#include <algorithm>
+#include <iterator>
 
 // User includes
 #include "VulcanSerial/Exception.hpp"
 #include "VulcanSerial/SerialPort.hpp"
 
-
+#define    BOTHER 0010000
 
 namespace VulcanSerial {
 
 	SerialPort::SerialPort() {
         echo_ = false;
         timeout_ms_ = defaultTimeout_ms_;
+		baudRateType_ = BaudRateType::STANDARD;
         baudRateStandard_ = defaultBaudRate_;
         readBufferSize_B_ = defaultReadBufferSize_B_;
         readBuffer_.reserve(readBufferSize_B_);
 		state_ = State::CLOSED;
 	}
 
-	SerialPort::SerialPort(const std::string& device, speed_t baudRate) :
+	SerialPort::SerialPort(const std::string& device, BaudRate baudRate) :
             SerialPort() {
 		device_ = device;
+		baudRateType_ = BaudRateType::STANDARD;
         baudRateStandard_ = baudRate;
 	}
 
 
-	SerialPort::SerialPort(const std::string& device, speed_t baudRate, NumDataBits numDataBits, Parity parity, NumStopBits numStopBits) :
+	SerialPort::SerialPort(const std::string& device, speed_t baudRate) :
             SerialPort() {
 		device_ = device;
+		baudRateType_ = BaudRateType::CUSTOM;
+        baudRateCustom_ = baudRate;
+	}
+
+	SerialPort::SerialPort(const std::string& device, BaudRate baudRate, NumDataBits numDataBits, Parity parity, NumStopBits numStopBits) :
+            SerialPort() {
+		device_ = device;
+		baudRateType_ = BaudRateType::STANDARD;
         baudRateStandard_ = baudRate;
 		numDataBits_ = numDataBits;
 		parity_ = parity;
@@ -58,9 +83,16 @@ namespace VulcanSerial {
         	ConfigureTermios();
 	}
 
+	void SerialPort::SetBaudRate(BaudRate baudRate)	{		
+		baudRateType_ = BaudRateType::STANDARD;
+		baudRateStandard_ = baudRate;
+        if(state_ == State::OPEN)
+            ConfigureTermios();
+	}
 
 	void SerialPort::SetBaudRate(speed_t baudRate)	{		
-		baudRateStandard_ = baudRate;
+		baudRateType_ = BaudRateType::CUSTOM;
+		baudRateCustom_ = baudRate;
         if(state_ == State::OPEN)
             ConfigureTermios();
 	}
@@ -86,9 +118,14 @@ namespace VulcanSerial {
 	void SerialPort::Open()
 	{
 
+		// std::cout << "Attempting to open COM port \"" << device_ << "\"." << std::endl;
+
 		if(device_.empty()) {
 			THROW_EXCEPT("Attempted to open file when file path has not been assigned to.");
 		}
+
+		// Attempt to open file
+		//this->fileDesc = open(this->filePath, O_RDWR | O_NOCTTY | O_NDELAY);
 
 		// O_RDONLY for read-only, O_WRONLY for write only, O_RDWR for both read/write access
 		// 3rd, optional parameter is mode_t mode
@@ -118,6 +155,7 @@ namespace VulcanSerial {
 
 		// termios tty = GetTermios();
 		termios2 tty = GetTermios2();
+
 		//================= (.c_cflag) ===============//
 
 		// Set num. data bits
@@ -177,8 +215,115 @@ namespace VulcanSerial {
 
         //===================== BAUD RATE =================//
 
-        tty.c_ispeed = baudRateStandard_;
-        tty.c_ospeed = baudRateStandard_;
+		// We used to use cfsetispeed() and cfsetospeed() with the B... macros, but this didn't allow
+		// us to set custom baud rates. So now to support both standard and custom baud rates lets
+		// just make everything "custom". This giant switch statement could be replaced with a map/lookup
+		// in the future
+		if (baudRateType_ == BaudRateType::STANDARD) {
+			tty.c_cflag &= ~CBAUD;
+			tty.c_cflag |= CBAUDEX;
+			switch(baudRateStandard_) {
+				case BaudRate::B_0:
+					tty.c_ispeed = 0;
+					tty.c_ospeed = 0;
+					break;
+				case BaudRate::B_50:
+					tty.c_ispeed = 50;
+					tty.c_ospeed = 50;
+					break;
+				case BaudRate::B_75:
+					tty.c_ispeed = 75;
+					tty.c_ospeed = 75;
+				    break;
+				case BaudRate::B_110:
+					tty.c_ispeed = 110;
+					tty.c_ospeed = 110;
+				    break;
+				case BaudRate::B_134:
+					tty.c_ispeed = 134;
+					tty.c_ospeed = 134;
+				    break;
+				case BaudRate::B_150:
+
+					tty.c_ispeed = 150;
+					tty.c_ospeed = 150;
+				    break;
+				case BaudRate::B_200:
+					tty.c_ispeed = 200;
+					tty.c_ospeed = 200;
+				    break;
+				case BaudRate::B_300:
+
+					tty.c_ispeed = 300;
+					tty.c_ospeed = 300;
+				    break;
+				case BaudRate::B_600:
+					tty.c_ispeed = 600;
+					tty.c_ospeed = 600;
+				    break;
+				case BaudRate::B_1200:
+					tty.c_ispeed = 1200;
+					tty.c_ospeed = 1200;
+				    break;
+				case BaudRate::B_1800:
+					tty.c_ispeed = 1800;
+					tty.c_ospeed = 1800;
+				    break;
+				case BaudRate::B_2400:;
+					tty.c_ispeed = 2400;
+					tty.c_ospeed = 2400;
+				    break;
+				case BaudRate::B_4800:
+					tty.c_ispeed = 4800;
+					tty.c_ospeed = 4800;
+				    break;
+				case BaudRate::B_9600:
+					tty.c_ispeed = 9600;
+					tty.c_ospeed = 9600;
+					break;
+				case BaudRate::B_19200:
+					tty.c_ispeed = 19200;
+					tty.c_ospeed = 19200;
+				    break;
+				case BaudRate::B_38400:
+					tty.c_ispeed = 38400;
+					tty.c_ospeed = 38400;
+				    break;
+				case BaudRate::B_57600:
+					tty.c_ispeed = 57600;
+					tty.c_ospeed = 57600;
+				    break;
+				case BaudRate::B_115200:
+					tty.c_ispeed = 115200;
+					tty.c_ospeed = 115200;
+				    break;
+				case BaudRate::B_230400:
+					tty.c_ispeed = 230400;
+					tty.c_ospeed = 230400;
+				    break;
+				case BaudRate::B_460800:
+					tty.c_ispeed = 460800;
+					tty.c_ospeed = 460800;
+				    break;
+				default:
+					throw std::runtime_error(std::string() + "baudRate passed to " + __PRETTY_FUNCTION__ + " unrecognized.");
+			}
+		}
+		// This does no different than STANDARD atm, but let's keep
+		// them separate for now....
+		else if (baudRateType_ == BaudRateType::CUSTOM)
+		{
+			tty.c_cflag &= ~CBAUD;
+			tty.c_cflag |= CBAUDEX;
+			// tty.c_cflag |= BOTHER;
+			tty.c_ispeed = baudRateCustom_;
+			tty.c_ospeed = baudRateCustom_;
+		}
+		else
+		{
+			// Should never get here, bug in this libraries code!
+			assert(false);
+		}
 
 		//===================== (.c_oflag) =================//
 
@@ -187,7 +332,19 @@ namespace VulcanSerial {
 
 		//================= CONTROL CHARACTERS (.c_cc[]) ==================//
 
+		// c_cc[VTIME] sets the inter-character timer, in units of 0.1s.
+		// Only meaningful when port is set to non-canonical mode
+        // VMIN = 0, VTIME = 0: No blocking, return immediately with what is available
+        // VMIN > 0, VTIME = 0: read() waits for VMIN bytes, could block indefinitely
+        // VMIN = 0, VTIME > 0: Block until any amount of data is available, OR timeout occurs
+        // VMIN > 0, VTIME > 0: Block until either VMIN characters have been received, or VTIME
+        //                      after first character has elapsed
+        // c_cc[WMIN] sets the number of characters to block (wait) for when read() is called.
+        // Set to 0 if you don't want read to block. Only meaningful when port set to non-canonical mode
+
         if(timeout_ms_ == -1) {
+            // Always wait for at least one byte, this could
+            // block indefinitely
             tty.c_cc[VTIME] = 0;
             tty.c_cc[VMIN] = 1;
         } else if(timeout_ms_ == 0) {
@@ -219,8 +376,23 @@ namespace VulcanSerial {
 		tty.c_lflag		&= ~ECHONL;								//
 		tty.c_lflag		&= ~ISIG;								// Disables recognition of INTR (interrupt), QUIT and SUSP (suspend) characters
 
+
+		// Try and use raw function call
+		//cfmakeraw(&tty);
+
+		// this->SetTermios(tty);
 		this->SetTermios2(tty);
 
+		/*
+		// Flush port, then apply attributes
+		tcflush(this->fileDesc, TCIFLUSH);
+
+		if(tcsetattr(this->fileDesc, TCSANOW, &tty) != 0)
+		{
+			// Error occurred
+			this->sp->PrintError(SmartPrint::Ss() << "Could not apply terminal attributes for \"" << this->filePath << "\" - " << strerror(errno));
+			return;
+		}*/
 	}
 
 	void SerialPort::Write(const std::string& data) {
@@ -240,36 +412,7 @@ namespace VulcanSerial {
 		}
 	}
 
-	
-    int SerialPort::Write(const std::string& data, uint16_t size) {
-
-        if(state_ != State::OPEN)
-            THROW_EXCEPT(std::string() + __PRETTY_FUNCTION__ + " called but state != OPEN. Please call Open() first.");
-
-		if(fileDesc_ < 0) {
-			THROW_EXCEPT(std::string() + __PRETTY_FUNCTION__ + " called but file descriptor < 0, indicating file has not been opened.");
-		}
-
-		int writeResult = write(fileDesc_, data.c_str(), size);
-
-        return writeResult;
-	}
-    
-    void SerialPort::WriteChar (const unsigned char c){
-        if(state_ != State::OPEN)
-            THROW_EXCEPT(std::string() + __PRETTY_FUNCTION__ + " called but state != OPEN. Please call Open() first.");
-
-		if(fileDesc_ < 0) {
-			THROW_EXCEPT(std::string() + __PRETTY_FUNCTION__ + " called but file descriptor < 0, indicating file has not been opened.");
-		}
-        int writeResult  = write (fileDesc_, &c, 1) ;
-        		// Check status
-		if (writeResult == -1) {
-			throw std::system_error(EFAULT, std::system_category());
-		}
-    }
-
-    void SerialPort::WriteBinary(const std::vector<uint8_t>& data) {
+       void SerialPort::WriteBinary(const std::vector<uint8_t>& data) {
 
         if(state_ != State::OPEN)
             THROW_EXCEPT(std::string() + __PRETTY_FUNCTION__ + " called but state != OPEN. Please call Open() first.");
@@ -291,9 +434,19 @@ namespace VulcanSerial {
         data.clear();
 
 		if(fileDesc_ == 0) {
+			//this->sp->PrintError(SmartPrint::Ss() << "Read() was called but file descriptor (fileDesc) was 0, indicating file has not been opened.");
+			//return false;
 			THROW_EXCEPT("Read() was called but file descriptor (fileDesc) was 0, indicating file has not been opened.");
 		}
 
+		// Allocate memory for read buffer
+//		char buf [256];
+//		memset (&buf, '\0', sizeof buf);
+
+		// Read from file
+        // We provide the underlying raw array from the readBuffer_ vector to this C api.
+        // This will work because we do not delete/resize the vector while this method
+        // is called
 		ssize_t n = read(fileDesc_, &readBuffer_[0], readBufferSize_B_);
 
 		// Error Handling
@@ -304,31 +457,23 @@ namespace VulcanSerial {
 
 		if(n > 0) {
 
+//			buf[n] = '\0';
+			//printf("%s\r\n", buf);
+//			data.append(buf);
             data = std::string(&readBuffer_[0], n);
 			//std::cout << *str << " and size of string =" << str->size() << "\r\n";
 		}
 
 		// If code reaches here, read must of been successful
 	}
-    
-    
-    int SerialPort::ReadChar()
-    {
-        uint8_t charBuf ;
 
-        if (read(fileDesc_, &charBuf, 1) != 1){
-            return -1 ;
-        }
-        
-        return ((int)charBuf) & 0xFF ;
-    }
-        
 	void SerialPort::ReadBinary(std::vector<uint8_t>& data)
 	{
         data.clear();
 
 		if(fileDesc_ == 0) {
-        
+			//this->sp->PrintError(SmartPrint::Ss() << "Read() was called but file descriptor (fileDesc) was 0, indicating file has not been opened.");
+			//return false;
 			THROW_EXCEPT("Read() was called but file descriptor (fileDesc) was 0, indicating file has not been opened.");
 		}
 
@@ -351,6 +496,40 @@ namespace VulcanSerial {
 		// If code reaches here, read must of been successful
 	}
 
+	// termios SerialPort::GetTermios() {
+    //     if(fileDesc_ == -1)
+    //         throw std::runtime_error("GetTermios() called but file descriptor was not valid.");
+
+	// 	struct termios tty;
+	// 	memset(&tty, 0, sizeof(tty));
+
+	// 	// Get current settings (will be stored in termios structure)
+	// 	if(tcgetattr(fileDesc_, &tty) != 0)
+	// 	{
+	// 		// Error occurred
+	// 		std::cout << "Could not get terminal attributes for \"" << device_ << "\" - " << strerror(errno) << std::endl;
+	// 		throw std::system_error(EFAULT, std::system_category());
+	// 		//return false;
+	// 	}
+
+	// 	return tty;
+	// }
+
+	// void SerialPort::SetTermios(termios myTermios)
+	// {
+	// 	// Flush port, then apply attributes
+	// 	tcflush(fileDesc_, TCIFLUSH);
+
+	// 	if(tcsetattr(fileDesc_, TCSANOW, &myTermios) != 0)
+	// 	{
+	// 		// Error occurred
+	// 		std::cout << "Could not apply terminal attributes for \"" << device_ << "\" - " << strerror(errno) << std::endl;
+	// 		throw std::system_error(EFAULT, std::system_category());
+
+	// 	}
+
+	// 	// Successful!
+	// }
 
 	termios2 SerialPort::GetTermios2()
 	{
@@ -359,6 +538,13 @@ namespace VulcanSerial {
         ioctl(fileDesc_, TCGETS2, &term2);
 
 		return term2;
+
+        // term2.c_cflag &= ~CBAUD;  /* Remove current BAUD rate */
+        // term2.c_cflag |= BOTHER;  /* Allow custom BAUD rate using int input */
+        // term2.c_ispeed = speed;   /* Set the input BAUD rate */
+        // term2.c_ospeed = speed;   /* Set the output BAUD rate */
+
+        // ioctl(fd, TCSETS2, &term2);
 	}
 
 	void SerialPort::SetTermios2(termios2 tty)
@@ -396,7 +582,6 @@ namespace VulcanSerial {
         return ret;
         
     }
-    
     State SerialPort::GetState() {
       return state_;
     }
