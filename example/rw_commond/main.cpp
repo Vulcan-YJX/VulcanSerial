89361196e469544352562430d58828c008e59345
vulcan-walker <56829006+Vulcan-YJX@users.noreply.github.com>
2022-06-11 15:42:31 +0800

    Update main.cpp

--- example/rw_commond/main.cpp
+++ example/rw_commond/main.cpp
@@ -8,7 +8,7 @@ using namespace VulcanSerial;
 int main() {
 
 	SerialPort serialPort("/dev/ttyUSB0", BaudRate::B_115200, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE);
-	serialPort.Open(timeout_ms = 0); 
+	serialPort.Open(0); 
 
 	while(1){
 		while(serialPort.Available() > 0){
