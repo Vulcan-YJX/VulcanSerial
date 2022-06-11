1cb17d1b4a3f13993dae1b79a38405a2eb008bb6
vulcan-walker <56829006+Vulcan-YJX@users.noreply.github.com>
2022-06-11 15:53:20 +0800

    Update SerialPort.hpp

--- include/VulcanSerial/SerialPort.hpp
+++ include/VulcanSerial/SerialPort.hpp
@@ -193,7 +193,7 @@ namespace VulcanSerial {
             /// \param      timeout_ms  Set to -1 to infinite timeout,or >0 to wait for data for a specified number of milliseconds). Timeout will
             ///             be rounded to the nearest 100ms (a Linux API restriction). Maximum value limited to
             ///             25500ms (another Linux API restriction).
-            void Open(int32_t timeout_ms);
+            void Open(int32_t timeout_ms = -1);
 
             /// \brief		Closes the COM port.
             void Close();
